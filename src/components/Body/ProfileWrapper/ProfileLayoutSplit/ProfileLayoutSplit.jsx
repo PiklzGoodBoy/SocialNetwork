a9bf3074ce6bb5ea449518b62aeea64f7f2e7866
Gleb Kokoulin <gogoaps87@gmail.com>
2024-07-10 21:39:48 +0300

    ProfileWell

--- src/components/Body/ProfileWrapper/ProfileLayoutSplit/ProfileLayoutSplit.jsx
+++ src/components/Body/ProfileWrapper/ProfileLayoutSplit/ProfileLayoutSplit.jsx
@@ -2,27 +2,12 @@ import React from 'react'
 import style from './ProfileLayoutSplit.module.css'
 
 const ProfileLayoutSplit = () => {
-  return (
-    <div>
-    <div className={style.SplitCol}>
-        <div>InternalGroup</div>
-        <div className={style.WallLegasy}>
-            <div>PageBlock</div>
-            <div>WallModule</div>
+    return (
+        <div className={style.ProfileLayoutSplit}>
+
+            
         </div>
-    </div>
-    <div className={style.ScrollStickyWrapper}>
-        <div className={style.ProfileFriends}>
-            Friends
-        </div>
-        <div className={style.ProfileSubscriptions}>
-            Subscriptions
-        </div>
-        <div className={style.ProfileVideos}>
-            Videos
-        </div>
-    </div>
-</div>  )
+    )
 }
 
 export default ProfileLayoutSplit
--- src/components/Body/ProfileWrapper/ProfileLayoutSplit/ProfileWell/ProfileWell.jsx
+++ src/components/Body/ProfileWrapper/ProfileLayoutSplit/ProfileWell/ProfileWell.jsx
@@ -3,7 +3,14 @@ import style from './ProfileWell.module.css';
 
 const ProfileWell = () => {
   return (
-    <div className={style.ProfileWell}>ProfileWell</div>
+    <div className={style.ProfileWell}>
+      <div className={style.SplitCol}>
+        <div>InternalGroup</div>
+        <div className={style.WallLegasy}>
+          <div>PageBlock</div>
+          <div>WallModule</div>
+        </div>
+      </div></div>
   )
 }
 

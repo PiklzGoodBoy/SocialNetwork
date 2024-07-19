d49e72552465b65749b3ce4cae6deed69627501f
Gleb Kokoulin <gogoaps87@gmail.com>
2024-07-19 12:18:39 +0300

    profile_transfer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.module.css';
 import Header from './components/Header/Header';
 import Profile from "./components/Profile/Profile";
 import style from './App.module.css';
+import LeftBar from './components/LeftBar/LeftBar';
 
 function App() {
   return (
     <div className={style.App}>
         <Header/>
         <Profile/>
+        <LeftBar/>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import style from './Profile.module.css';
-import LeftBar from './LeftBar/LeftBar';
 import ProfileWrapper from './ProfileWrapper/ProfileWrapper';
 
 
 function Profile(props) {
     return (
         <div className={style.Profile}>
-
-           <LeftBar/>
            <ProfileWrapper/>
-            
         </div>
     );
 }

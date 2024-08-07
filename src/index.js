d657e327e7e5d80544d7059d0afb397aa4460e14
Gleb Kokoulin <gogoaps87@gmail.com>
2024-08-07 14:12:37 +0300

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -3,33 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-
-let postData = [
-  { id: 1, message: "Hi, how are you?", likeCount: 11 },
-  { id: 2, message: "It's my firtst project", likeCount: 12 }
-]
-
-let DialogsData = [
-  { id: 1, name: 'Dima' },
-  { id: 2, name: 'Masha' },
-  { id: 3, name: 'Sasha' },
-  { id: 4, name: 'Anton' },
-  { id: 5, name: 'Baton' },
-  { id: 6, name: 'Vosik' }
-]
-
-let MessagesData = [
-  { id: 1, message: 'Hi' },
-  { id: 2, message: 'Welcome to Abab' },
-  { id: 3, message: 'WTF?' },
-  { id: 4, message: 'He he he' },
-  { id: 5, message: 'Wou' },
-  { id: 5, message: 'You Boy' }
-]
-=======
 import state from './redux/state';
->>>>>>> cef0295 (state:Profile,Dialogs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

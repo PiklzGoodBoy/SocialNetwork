a4d38150526dcae8992c9bd4a4d1590a1b63a415
Gleb Kokoulin <gogoaps87@gmail.com>
2024-08-14 15:20:35 +0300

    AddOnClick

--- src/components/Profile/ProfileWrapper/ProfileLayoutSplit/ProfileWall/SubmitPostBox/SubmitPostBox.jsx
+++ src/components/Profile/ProfileWrapper/ProfileLayoutSplit/ProfileWall/SubmitPostBox/SubmitPostBox.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 import style from './SubmitPostBox.module.css'
 
 
-export default function SubmitPostBox  ()  {
+export default function SubmitPostBox() {
+  
+  let addPost=()=>{
+    alert('Samurai')
+  };
+  
+  
+  
   return (
-    <div className={style.SubmitPostBox}>SubmitPostBox</div>
+    <div className={style.SubmitPostBox}>
+
+      <div>
+        <textarea >
+      </textarea>
+      </div>
+      <button onClick={addPost}>Add post</button>
+
+    </div>
   )
 }

e41725e928e20eb68ee8054938d712ba54ceb664
Gleb Kokoulin <gogoaps87@gmail.com>
2024-08-14 19:02:57 +0300

    Update CSS:App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Music from './components/Music/Music';
 import Videos from './components/Videos/Videos';
 import Games from './components/Games/Games';
 
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App(props) {
@@ -20,21 +19,26 @@ export default function App(props) {
     <BrowserRouter>
       <div className={style.app_wrapper}>
         <Header />
-        <LeftBar />
-        <div className={style.app_wrapper_content}>
-          <Routes>
-            <Route path='/' element={<Profile />} />
-            <Route path='profile' element={<Profile state={props.state.profilePage} />}
-            />
-            <Route path='dialogs' element={<Dialogs state={props.state.dialogsPage} />}
-            />
-            <Route path='news' element={<News />} />
-            <Route path='friends' element={<Friends />} />
-            <Route path='community' element={<Community />} />
-            <Route path='music' element={<Music />} />
-            <Route path='videos' element={<Videos />} />
-            <Route path='games' element={<Games />} />
-          </Routes>
+        <div className={style.content}>
+          <span />
+          <LeftBar />
+          <div className={style.app_wrapper_content}>
+            <Routes>
+              <Route path='/' element={<Profile state={props.state.profilePage} />} />
+              <Route path='profile' element={<Profile state={props.state.profilePage} />}
+              />
+              <Route path='dialogs' element={<Dialogs state={props.state.dialogsPage} />}
+              />
+              <Route path='news' element={<News />} />
+              <Route path='friends' element={<Friends />} />
+              <Route path='community' element={<Community />} />
+              <Route path='music' element={<Music />} />
+              <Route path='videos' element={<Videos />} />
+              <Route path='games' element={<Games />} />
+            </Routes>
+          </div>
+          <span />
+
         </div>
       </div>
     </BrowserRouter>

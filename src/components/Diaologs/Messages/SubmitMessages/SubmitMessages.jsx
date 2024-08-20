8d5a5abfe7c3de2b1ea56f515ec9a0fb4e80f7ef
Gleb Kokoulin <gogoaps87@gmail.com>
2024-08-20 16:27:42 +0300

    SubmitMessages:createRef

--- src/components/Diaologs/Messages/SubmitMessages/SubmitMessages.jsx
+++ src/components/Diaologs/Messages/SubmitMessages/SubmitMessages.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import style from './SubmitMessages.module.css'
 
-export default function SubmitMessages  ()  {
+export default function SubmitMessages(props) {
+
+  let newMessElement = React.createRef();
+  let addMess = () => {
+    let text = newMessElement.current.value;
+    alert(text)
+  }
+
   return (
     <div className={style.SubmitMessages}>
-        <textarea></textarea>
-        <button >Add post</button>
+      <textarea ref={newMessElement}></textarea>
+      <button onClick={addMess}>Add post</button>
     </div>
   )
 }
